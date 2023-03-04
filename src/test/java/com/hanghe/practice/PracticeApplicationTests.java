8e1b8f42cd06fb3232596e32c86725377698b194
akrwkdrrr99 <akrwkdrrr99@gmail.com>
2023-03-05 05:00:44 +0900

    기본 세팅잡기

---
+++ src/test/java/com/hanghe/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghe.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
